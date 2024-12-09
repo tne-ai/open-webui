0e7982f2c36e638c938417436edac1ac73c57db7
Lucas Hahn <lhahn1@me.com>
2024-12-09 15:23:01 -0500

    Add tne-agent-v2 imports

--- tne-agent-v2/lib/agents/python/python_code_agent.d.ts
+++ tne-agent-v2/lib/agents/python/python_code_agent.d.ts
@@ -3,7 +3,14 @@ type ParamInput = {
     code?: string;
     data?: unknown;
 };
+type OnError = {
+    onError: {
+        message: string;
+        status?: number;
+        details?: string | Record<string, unknown>;
+    };
+};
 type ResultData = Record<string, never>;
-export declare const pythonCodeAgent: AgentFunction<ParamInput, ResultData, ParamInput>;
+export declare const pythonCodeAgent: AgentFunction<ParamInput, ResultData | OnError, null, ParamInput>;
 declare const pythonCodeAgentInfo: AgentFunctionInfo;
 export default pythonCodeAgentInfo;
