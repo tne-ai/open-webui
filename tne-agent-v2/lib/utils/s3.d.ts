1bcd1420c635326d59733de89771b00d192ea797
Lucas Hahn <lhahn1@me.com>
2024-12-09 15:23:01 -0500

    Add tne-agent-v2 imports

--- tne-agent-v2/lib/utils/s3.d.ts
+++ tne-agent-v2/lib/utils/s3.d.ts
@@ -1,6 +1,6 @@
 import { S3Data } from "../types";
 export { S3Data };
-export declare const fetchS3File: (uid: string, fileName: string, region?: string, bucket?: string, verbose?: boolean) => Promise<Uint8Array | undefined>;
+export declare const fetchS3File: (uid: string, fileName: string, region?: string, bucket?: string, verbose?: boolean) => Promise<Uint8Array<ArrayBufferLike> | undefined>;
 export declare const writeS3File: (uid: string, fileName: string, Body: string | undefined, region?: string, bucket?: string, verbose?: boolean) => Promise<import("@aws-sdk/client-s3").PutObjectCommandOutput>;
 export declare const buffer2returnData: (data: Buffer, fileName: string) => S3Data;
 export declare const fileName2imageType: (fileName: string) => "jpeg" | "png" | "gif" | "unknown";
