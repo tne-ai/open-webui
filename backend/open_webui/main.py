98730e951484ac9d7df848ac1fb3381fb89d1ff3
Lucas Hahn <lhahn1@me.com>
2025-01-07 13:11:04 -0500

    Remove conflicting middleware

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -685,7 +685,7 @@ class RedirectMiddleware(BaseHTTPMiddleware):
 app.add_middleware(RedirectMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174", "http://localhost:8080"],
+    allow_origins=["http://localhost:5174", "http://localhost:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -725,15 +725,6 @@ async def inspect_websocket(request: Request, call_next):
                 content={"detail": "Invalid WebSocket upgrade request"},
             )
     return await call_next(request)
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=CORS_ALLOW_ORIGIN,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 app.mount("/ws", socket_app)
